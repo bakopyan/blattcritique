a373d9889e8cbe2f2e64a3f1d02734b9dea6efe9
Bagrat Akopyan <bagrat.akopyan@gmail.com>
2025-03-22 11:27:50 +0100

    modified about page.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,15 +1,21 @@
-<script>
-    import { page } from '$app/stores';
-</script>
+<div>
     <h2>Impressum und ViSdP</h2>
     <p>your name</p>
-    <p>your org</p>
-    <p>your address</p>
-    <p>your city</p>
-    <p>your country</p>
-    <p>your email</p>
-    <p>your phone</p>
-    <p>your website</p>
-
-    
+    <p>info (at) blattcritique (punkt) de</p>
+</div>
+<div>
     <h2>Datenschutzerklärung</h2>
+    <p>TODO...</p>
+</div>
+
+<style>
+    div {
+        margin: 0 0 3rem 0; /* top right bottom left */
+    }
+    h2 {
+        margin-bottom: 0.5rem;
+    }
+    p {
+        margin: 0;
+    }
+</style>
